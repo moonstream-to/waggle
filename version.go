e5d528a5b83614bf8fa4953bf5a4070a82397171
Neeraj Kashyap <nkashy1@gmail.com>
2023-08-18 20:02:08 -0700

    Reverted WaggleVersion() to WAGGLE_VERSION
    Better for there to be a single source of truth for version inside a go
    source file.

--- version.go
+++ version.go
@@ -1,16 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-)
-
-func WaggleVersion() string {
-	version, err := ioutil.ReadFile("version.txt")
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	return string(version)
-}
+var WAGGLE_VERSION = "0.0.9"
